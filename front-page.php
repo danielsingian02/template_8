65d665a92919a4a195cbf772f20ca55bbc6ea85d
Daniel Singian <daniel.singian02@gmail.com>
2022-11-04 17:28:32 +0800

    Add mobile responsiveness to the front-page

--- front-page.php
+++ front-page.php
@@ -19,6 +19,8 @@
 				<p>
 					<?php printf( get_theme_mod( 'second_section_paragraph', __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque justo metus, auctor non consequat vel, iaculis ut ante. Nulla vitae condimentum libero, ac pretium lectus. Pellentesque eget turpis tempus, accumsan odio ac, suscipit ex. Sed malesuada, leo posuere rutrum viverra, turpis neque ornare augue, vitae convallis dolor libero vitae lectus. Donec in dictum neque. Nunc tempor, odio nec sodales lacinia, tellus urna viverra nulla, ac lacinia leo erat id erat. Etiam accumsan cursus ipsum iaculis dapibus. Praesent viverra vitae velit sit amet maximus', 'second_section_paragraph' ) ) ); ?>
 				</p>
+
+				<img class="content-text-img" src="<?php echo get_theme_mod('second_image',get_bloginfo('template_directory').'/assets/images/Asset 1 template 8 1.png' )?>" border="0" alt="">
 			</div>
 			
 		</div>
@@ -33,11 +35,11 @@
 				<p class="context-text__text">
 					<?php printf( get_theme_mod( 'description', __( 'Donec in dictum neque. Nunc tempor, odio nec sodales lacinia, tellus urnaviverra nulla, ac lacinia leo erat id erat. Etiam accumsan cursus ipsumiaculis dapibus. Praesent viverra vitae velit sit amet maximus <br><br><b><span>Backyard Sale 12-15-2021 <br>Easter Egg Hunt 04-23-2021 <br>Coffee Hours 02-08-2021 <br>Backyard Together 11-16-2021 <br>Pet Celebration 07-10-2021</span></b>', 'description' ) ) ); ?>
 				</p>
+
+				<img class="content-text-img" src="<?php echo get_theme_mod('second_image',get_bloginfo('template_directory').'/assets/images/Asset 2 1.png' )?>" border="0" alt="">
 			</div>
 		</div>
 	</div>
-
-		
 
 	<div class="house">
 		<img class="houses-img" src="<?php echo get_theme_mod( 'whole_image', get_bloginfo( 'template_directory' ) . '/assets/images/Asset 3 (1).png' ); ?>" border="0" alt="">
